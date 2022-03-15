76c96685f0cef4f8660dd8d71fc7bdc09f23d6d0
Christine Mwangi <christine.mwangi@student.moringaschool.com>
2022-03-15 16:27:57 +0300

    feat:added business logic

--- script.js
+++ script.js
@@ -21,4 +21,21 @@ function Contact(first, last) {
       });
       
   });
+
+  $(".contact").last().click(function() {
+    $("#show-contact").show();
+    $("#show-contact h2").text(newContact.firstName);
+    $(".first-name").text(newContact.firstName);
+    $(".last-name").text(newContact.lastName);
+  }); 
+  
+  // business logic
+function Contact(first, last) {
+    this.firstName = first;
+    this.lastName = last;
+  }
+  
+  Contact.prototype.fullName = function() {
+    return this.firstName + " " + this.lastName;
+  }
   
